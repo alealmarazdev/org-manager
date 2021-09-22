387c14ebce556bd1483d85324a95300379283f53
alealmarazdev <alejandra.almaraz23@gmail.com>
2021-09-22 14:45:11 -0500

    ADD users view

---
+++ components/DrawerDetailUser.tsx
@@ -0,0 +1,42 @@
+import { Button, Col, Divider, Drawer, Row, Space, Typography } from "antd"
+import React, { FC } from "react"
+import DescriptionItem from "./DescriptionItem";
+
+const { Title } = Typography;
+
+type Props = {
+
+  }
+
+const DrawerDetailUser: FC<Props> =() => {
+    return (
+      
+          <>
+          <Row>
+            <Col span={24}>
+                <DescriptionItem title="Full Name" content="Lily Potter" />
+            </Col>
+            <Divider />
+            <Col span={24}>
+                <DescriptionItem title="Email" content="email1@example.com" />
+            </Col>
+            <Divider />
+            <Col span={24}>
+                <DescriptionItem title="English level" content="Middle" />
+            </Col>
+            <Divider />
+            <Col span={24}>
+                <DescriptionItem title="Resume Link" content="China🇨🇳" />
+            </Col>
+        </Row><Divider /><Row>
+                <Col span={24}>
+                    <DescriptionItem
+                        title="Skills"
+                        content="C / C + +, data structures, software engineering, operating systems, computer networks, databases, compiler theory, computer architecture, Microcomputer Principle and Interface Technology, Computer English, Java, ASP, etc." />
+                </Col>
+            </Row><Divider /></>
+         
+        );
+    };
+    
+    export default DrawerDetailUser
--- pages/users.tsx
+++ pages/users.tsx
@@ -1,12 +1,119 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import { Button, Divider, Drawer, Radio, Space, Table, Typography } from 'antd';
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+
 import Layout from '../components/LayoutTemplate';
+import DrawerForm from '../components/DrawerForm';
+import DrawerDetailUser from '../components/DrawerDetailUser';
+
+import dataUsers from '../utils/dataUsers';
+
+const { Title } = Typography;
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Users: FC = () => {
+  const [visible, setVisible] = useState(false)
+  const [userDetail, setUserDetail] = useState(false)
+
+  const handleState = () => {
+    setUserDetail(true)
+    setVisible(!visible)
+    console.log(userDetail)
+  }
+   
+  const showDrawerForm = () => {
+    setUserDetail(false)
+    setVisible(!visible)
+  };
+
+  const handleEdit = (e: any) => {
+    setUserDetail(false)
+    setVisible(!visible)
+    console.log('edit' + e)
+  }
+
+  const handleDelete = (e: any) => {
+    console.log('delete' + e)
+  }
 
 
-export default function Users() {
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      render: (text: string) => <Button onClick={handleState} type="link">{text}</Button>,
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      
+    },
+    {
+      title: 'Password',
+      dataIndex: 'password',
+     
+    },
+    {
+      title: () => <EditOutlined />,
+      dataIndex: 'edit',
+      render : () => <Button onClick={handleEdit} type="text" ><EditOutlined /></Button>,
   
+    },
+    {
+      title: () => <DeleteOutlined />,
+      dataIndex: 'delete',
+      render : () => <Button onClick={handleDelete} type="text"><DeleteOutlined /></Button>,
+   
+    },
+    
+  ];
+
   return (
     <Layout title="users" description="users">
-     <p>users</p>
+     <Title level={3}>USERS</Title>
+     <Divider /> 
+     <div>
+      <Table
+        columns={columns}
+        dataSource={dataUsers.users}
+        scroll={{ x: "max-content", y: "max-content" }}
+      />
+    </div>
+
+    <Button type="primary" onClick={showDrawerForm} icon={<PlusOutlined />}>
+      Add a new user
+    </Button>
+    <Drawer
+    title={userDetail ? 'User Profile' :  `Create a new user `}
+    width={720}
+    onClose={handleState}
+    visible={visible}
+    bodyStyle={{ paddingBottom: 80 }}
+     /* @ts-ignore */ 
+    extra={
+      <Space>
+        <Button onClick={handleState}>Cancel</Button>
+        <Button onClick={handleState} type="primary">
+          Submit
+        </Button>
+      </Space>
+    }
+  >
+    {
+      userDetail && <DrawerDetailUser /> 
+    }
+      {!userDetail && <DrawerForm />
+    }
+    </Drawer>
     </Layout>
   );
 }
+
+
+export default Users
