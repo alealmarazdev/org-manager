5d97ba2d6c4aa8ef7bf62bf4fa2a7d1049b42b6e
alealmarazdev <alejandra.almaraz23@gmail.com>
2021-09-22 15:03:58 -0500

    ADD mocks

--- utils/data.js
+++ utils/data.js
@@ -1,12 +1,15 @@
 const data = {
   flow: [
     {
+      list: 'Users',
       link: 'users',
     },
     {
+      list: 'Accounts',
       link: 'accounts',
     },
     {
+      list: 'Teams',
       link: 'teams',
     },
   ],
---
+++ utils/dataAccount.js
@@ -0,0 +1,36 @@
+import dataTeam from './dataTeams';
+
+const dataAccounts = {
+  accounts: [
+    {
+      key: '1',
+      account: 'Brown',
+      client: 'Client1',
+      responsable: 'John Brown',
+      team: 'rio',
+    },
+    {
+      key: '2',
+      account: 'Green',
+      client: 'CCCCC2',
+      responsable: 'Jim Green',
+      team: 'rio',
+    },
+    {
+      key: '3',
+      account: 'Black',
+      client: 'Client3',
+      responsable: 'Joe Black',
+      team: 'rio',
+    },
+    {
+      key: '4',
+      account: 'Harper',
+      client: 'Client4',
+      responsable: 'Joshep Harper',
+      team: 'rio',
+    },
+  ],
+};
+
+export default dataAccounts;
