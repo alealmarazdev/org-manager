087780d6c480a8e28f2298cc582a669b952bd90d
alealmarazdev <alejandra.almaraz23@gmail.com>
2021-09-22 15:07:04 -0500

    ADD team template

---
+++ components/DrawerDetailTeam.tsx
@@ -0,0 +1,41 @@
+import { Button, Col, Divider, Drawer, Row, Space, Typography } from "antd"
+import React, { FC } from "react"
+import DescriptionItem from "./DescriptionItem";
+
+const { Title } = Typography;
+interface DataType {
+    key: React.Key;
+    account: string;
+    client: string;
+    responsable: string;
+    team: string[];
+  
+  }
+  
+
+type Props = {
+
+  }
+
+const DrawerDetailTeam: FC<Props> =() => {
+    return (
+      
+          <>
+          <Row>
+            <Col span={24}>
+                <DescriptionItem title="Team Name" content="Rio" />
+            </Col>
+           
+            <Col span={24}>
+                <DescriptionItem title="Email" content="email1@example.com" />
+            </Col>
+            <Divider />
+           
+            </Row>
+            <Divider />
+            </>
+         
+        );
+    };
+    
+    export default DrawerDetailTeam
--- pages/teams.tsx
+++ pages/teams.tsx
@@ -1,12 +1,66 @@
 import React from 'react';
+import { Button, Divider, Radio, Table, Typography } from 'antd';
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+
 import Layout from '../components/LayoutTemplate';
+import dataUsers from '../utils/dataUsers';
+
+const { Title } = Typography;
+const columns = [
+  {
+    title: 'Key',
+    dataIndex: 'key',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Teams',
+    dataIndex: 'teams.name',
+    
+  },{
+    title: 'Name',
+    dataIndex: 'name',
+    
+  },
+  
+  {
+    title: () => <EditOutlined />,
+    dataIndex: 'edit',
+    render : () => <EditOutlined />,
+
+  },
+  {
+    title: () => <DeleteOutlined />,
+    dataIndex: 'delete',
+    render : () => <DeleteOutlined />,
+ 
+  },
+  
+];
+
+interface DataType {
+  key: React.Key;
+  name: string;
+
+}
 
 
 export default function Teams() {
-  
+  console.log(dataUsers.users)
   return (
     <Layout title="teams" description="teams">
-     <p>teams</p>
+     <Title level={3}>TEAMS</Title>
+     <Divider /> 
+     <div>
+      <Table
+        columns={columns}
+        dataSource={dataUsers.users}
+        scroll={{ x: "max-content", y: "max-content" }}
+      />
+    </div>
+
+    <Button type="primary" icon={<PlusOutlined />}>
+      Add a new team
+    </Button>
     </Layout>
   );
 }
